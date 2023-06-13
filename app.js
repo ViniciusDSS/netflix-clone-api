23ffd07630d2716d7a95f28442f3f2b0eca67687
Vinicius Domingues Silva <91794441+ViniciusDSS@users.noreply.github.com>
2023-06-12 21:56:11 -0300

    att

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const movieId = 123; // ID do filme que você deseja obter detalhes
 const apiKey = '431d906c5c1aab1ce9d89f91aceb0dd7';
 
+
 fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}`)
   .then(response => response.json())
   .then(data => {
